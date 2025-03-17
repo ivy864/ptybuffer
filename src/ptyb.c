9f6da9d1c60ae3c260c778abe8fea5906000ebc5
jayers <jayers3@mail.umw.edu>
2025-03-17 11:34:45 -0400

    run ptyb client

--- src/ptyb.c
+++ src/ptyb.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include "./client/client.h"
 
 int main(int argc, char *argv[])
 {
-    printf("meow");
+    if (argc == 2 && strcmp(argv[1], "init") == 0) {
+        return init_client();
+    }
     return EXIT_SUCCESS;
 }
