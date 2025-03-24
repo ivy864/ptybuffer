b8832da5956b55738f51b8ed2a17449a05dbc32d
jayers <jayers3@mail.umw.edu>
2025-03-24 16:47:19 -0400

    socket stuff tests

--- src/ptyb.c
+++ src/ptyb.c
@@ -2,11 +2,19 @@
 #include <stdio.h>
 #include <string.h>
 #include "./client/client.h"
+#include "./server/server.h"
 
 int main(int argc, char *argv[])
 {
-    if (argc == 2 && strcmp(argv[1], "init") == 0) {
-        return init_client();
+    if (argc == 2) {
+        if (strcmp(argv[1], "init") == 0) {
+            server_connect("/home/horse/Repos/ptybuffer/sock");
+            // return init_client();
+        }
+        else if (strcmp(argv[1], "start_server") == 0) {
+            return init_server("/home/horse/Repos/ptybuffer/sock");
+        }
     }
+
     return EXIT_SUCCESS;
 }
