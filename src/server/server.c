e972f0202d9e7eb65e51e690ef588ca0059ad355
jayers <jayers3@mail.umw.edu>
2025-03-27 11:43:36 -0400

    add start_server function to start server in new process

--- src/server/server.c
+++ src/server/server.c
@@ -4,41 +4,37 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
+#include "server.h"
 
 #define __USE_MISC
 #include <sys/un.h>
 
-int init_server(char *socket_domain) {
-    struct sockaddr_un addr;
-    socklen_t addrlen;
+// alocate uninitialized server
+PtybServer *server_alloc() {
+    PtybServer *server = malloc(sizeof(PtybServer));
+    server->addr = malloc(sizeof(struct sockaddr_un));
+
+    return server;
+}
+
+/**
+ * Free server and close socket.
+ */
+void server_free(PtybServer *server) {
+    free(server->addr);
+    close(server->sock);
+    free(server);
+}
+
+int server_main(PtybServer *server) {
     char buffer [1024];
 
-    int sock = socket(AF_LOCAL, SOCK_STREAM, 0);
-    if (sock < 0) {
-        fprintf(stderr, "Error %d on socket()\n", errno);
-        exit(EXIT_FAILURE);
-    }
+    listen(server->sock, 5);
 
-    if (strlen(socket_domain) > sizeof(addr.sun_path) - 1) {
-        fprintf(stderr, "Server socket path too long.");
-        exit(EXIT_FAILURE);
-    }
+    socklen_t addr_len = SUN_LEN(server->addr);
 
-    memset(&addr, 0, sizeof(struct sockaddr_un));
-    addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, socket_domain, sizeof(addr.sun_path));
-
-    if (bind(sock, (struct sockaddr *) &addr, SUN_LEN(&addr)) < 0) {
-        fprintf(stderr, "Error %d on bind()\n", errno);
-        exit(EXIT_FAILURE);
-    }
-
-    addrlen = sizeof(addr.sun_path);
-
-    listen(sock, 5);
-
-    //while (1) {
-        int connection = accept(sock, (struct sockaddr *) &addr, &addrlen);
+    while (1) {
+        int connection = accept(server->sock, (struct sockaddr *) server->addr, &addr_len);
 
         if (connection < 0) {
             fprintf(stderr, "Error %d on connection()\n", errno);
@@ -52,7 +48,43 @@ int init_server(char *socket_domain) {
         printf("%s\n", buffer);
 
         close(connection);
-    //}
+    }
 
-    return EXIT_SUCCESS; 
+    return 2;
+}
+
+PtybServer *init_server(char *socket_domain) {
+    PtybServer *server = server_alloc();
+
+    server->sock = socket(AF_LOCAL, SOCK_STREAM, 0);
+
+    if (server->sock < 0) {
+        fprintf(stderr, "Error %d on socket()\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    if (strlen(socket_domain) > sizeof(server->addr->sun_path) - 1) {
+        fprintf(stderr, "Server socket path too long.");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(server->addr, 0, sizeof(struct sockaddr_un));
+    server->addr->sun_family = AF_UNIX;
+    strncpy(server->addr->sun_path, socket_domain, sizeof(server->addr->sun_path));
+
+    if (bind(server->sock, (struct sockaddr *) &server->addr, SUN_LEN(server->addr)) < 0) {
+        fprintf(stderr, "Error %d on bind()\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    return server;
+}
+
+int start_server(char *domain) {
+    if (fork() == 0) {
+        PtybServer *server = init_server(domain);
+        exit(server_main(server));
+    }
+
+    return 0;
 }
--- src/server/server.h
+++ src/server/server.h
@@ -1,6 +1,21 @@
 #ifndef PTYB_SERVER_INCLUDED
 #define PTYB_SERVER_INCLUDED
 
-int init_server(char *socket_domain);
+#define __USE_MISC
+#include <sys/un.h>
+
+typedef struct PtybServer PtybServer;
+
+struct PtybServer {
+    int sock; // socket file descriptor 
+    struct sockaddr_un *addr; // socket address
+};
+
+PtybServer *init_server(char *socket_domain);
+
+/**
+ * Initialize server and fork to new process.
+ */
+int start_server(char *domain);
 
 #endif
