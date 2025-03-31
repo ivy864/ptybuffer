dd6e233da292d266108c45ea432fafeab8849a06
jayers <jayers3@mail.umw.edu>
2025-03-30 23:40:40 -0400

    add server functions to utils

--- src/util/utils.c
+++ src/util/utils.c
@@ -2,6 +2,45 @@
 #include <limits.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <stdio.h>
+
+#define __USE_MISC
+#include <sys/un.h>
+
+PtybServer *ptybserver_alloc() {
+    PtybServer *server = malloc(sizeof(PtybServer));
+    server->addr = malloc(sizeof(struct sockaddr_un));
+
+    return server;
+}
+
+void ptybserver_free(PtybServer *server) {
+    free(server->addr);
+    close(server->sock);
+    free(server);
+}
+
+void ptybserver_sock_init(PtybServer *server, char *socket_domain) {
+    server->sock = socket(AF_LOCAL, SOCK_STREAM, 0);
+
+    if (server->sock < 0) {
+        fprintf(stderr, "Error %d on socket()\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    if (strlen(socket_domain) > sizeof(server->addr->sun_path) - 1) {
+        fprintf(stderr, "Server socket path too long.");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(server->addr, 0, sizeof(struct sockaddr_un));
+    server->addr->sun_family = AF_UNIX;
+    strncpy(server->addr->sun_path, socket_domain, sizeof(server->addr->sun_path));
+
+}
 
 IntList *intlist_init(int32_t size) {
     IntList *newlist = malloc(sizeof(IntList));
--- src/util/utils.h
+++ src/util/utils.h
@@ -4,6 +4,28 @@
 #include <stddef.h>
 #include <stdint.h>
 
+typedef struct PtybServer PtybServer;
+
+struct PtybServer {
+    int sock; // socket file descriptor 
+    struct sockaddr_un *addr; // socket address
+};
+
+/**
+ * Allocate memmory for PtybServer object
+ */
+PtybServer *ptybserver_alloc();
+
+/**
+ * Free PtybServer
+ */
+void ptybserver_free(PtybServer *server);
+
+/**
+ * Open socket in at sock_domain.
+ */
+void ptybserver_sock_init(PtybServer *server, char *sock_domain);
+
 typedef struct IntList IntList;
 
 /**
