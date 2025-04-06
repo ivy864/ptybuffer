eae0abd2f5723167d87513d345c54401261b555f
jayers <jayers3@mail.umw.edu>
2025-04-06 17:32:04 -0400

    fix client doing thigns wrong

--- src/client/client.c
+++ src/client/client.c
@@ -1,46 +1,73 @@
 #define _XOPEN_SOURCE 700
 
+#include <string.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
+#include <sys/select.h>
 
 #define __USE_MISC
 #include "client.h"
 #include <termios.h>
+#include <sys/ioctl.h>
 
 int client_main(int master) {
     char input[1024];
     int rc = 0;
+    printf("main started\n");
 
+    fd_set fd_in;
+    
     while (1) {
-        // use write instead of printf so it actually flushes
-        // write(1, "Input: ", sizeof("Input: "));
-        rc = read(0, input, sizeof(input));
+        FD_ZERO(&fd_in);
+        FD_SET(0, &fd_in);
+        FD_SET(master, &fd_in);
 
-        if (rc > 0) {
-            write(master, input, rc);
-            rc = read(master, input, sizeof(input) - 1);
-            if (rc > 0) {
-                input[rc] = '\0';
+        rc = select(master + 1, &fd_in, NULL, NULL, NULL);
 
-                ptyb_message_server("/run/user/1000/ptyb.sock", input);
-                fprintf(stderr, "%s", input);
-            }
-            else {
-                break;
-            }
+        if (rc == -1) {
+            fprintf(stderr, "Error %d on select()\n%s\n", errno, strerror(errno));
+            exit(1);
         }
         else {
-            break;
+            if (FD_ISSET(0, &fd_in)) {
+                rc = read(0, input, sizeof(input));
+                if (rc > 0) {
+                    write(master, input, rc);
+                    write(1, input, rc);
+                }
+                else {
+                    if (rc < 0) {
+                        fprintf(stderr, "Error %d on read stdin\n%s\n", errno, strerror(errno));
+                        exit(1);
+                    }
+                }
+            }
+
+            if (FD_ISSET(master, &fd_in)) {
+                rc = read(master, input, sizeof(input));
+                if (rc > 0) {
+                    write(1, input, rc);
+                }
+                else {
+                    if (rc < 0) {
+                        fprintf(stderr, "Error %d on read master PTY\n%s\n", errno, strerror(errno));
+                        exit(1);
+                    }
+                }
+            }
         }
     }
+
 
     return EXIT_SUCCESS;
 }
 
 int init_shell(int maid) {
+    printf("???\n");
     struct termios maid_orig_term_settings;
     struct termios new_term_settings;
 
@@ -61,32 +88,31 @@ int init_shell(int maid) {
     dup(maid);
     dup(maid);
 
+    close(maid);
+
+    setsid();
+
+    ioctl(0, TIOCSCTTY, 1);
+
     /* bash: cannot set terminal process group (5908): Inappropriate ioctl for
      * device bash: no job control in this shell */
 
-    execl("/bin/bash", "bash");
-    /*
-    while (1) {
-        rc = read(maid, input, sizeof(input) - 1);
-
-        if (rc > 0) {
-            input[rc - 1] = '\0';
-            printf("Child received: '%s'\n", input);
-        }
-        else {
-            break;
-        }
+    {
+        execlp("bash", "bash");
     }
-    */
-    return EXIT_SUCCESS;
+    printf("...?\n");
+
+    return(EXIT_FAILURE);
 }
 
 /* code modified from https://www.rkoucha.fr/tech_corner/pty_pdip.html */
 
 /* initialize stuff and fork to client_main? */
-int init_client() {
+int init_client(char *sock_domain) {
     // 'maid' is used in place of 'slave' because I think slave is a weird name
     int master, maid, rc;
+    //uint32_t cid = ptyb_get_cid(sock_domain);
+    //printf("client recieved cid %d.\n", cid);
 
     master = posix_openpt(O_RDWR);
     if (master < 0) {
@@ -106,15 +132,20 @@ int init_client() {
         return EXIT_FAILURE;
     }
 
+
+    printf("meow\n");
     maid = open(ptsname(master), O_RDWR);
+    printf("maid: %d\n", maid);
 
     if (fork()) {
+        close(maid);
+        client_main(master);
+    }
+    else {
         close(master);
-        init_shell(maid);
+        printf("%d\n", init_shell(maid));
+        printf("huh\n");
     }
 
-    close(maid);
-    client_main(master);
-
-    return 0;
+    exit(0);
 }
--- src/client/client.h
+++ src/client/client.h
@@ -1,12 +1,14 @@
 #ifndef PTYB_CLIENT_INCLUDED
 #define PTYB_CLIENT_INCLUDED
 
-int ptyb_message_server(char *socket_domain, char *message);
-int init_client();
+int init_client(char *sock_domain);
 
 int client_main(int master);
 int init_shell(int maid);
 
-int server_connect(char *socket_domain);
+int server_connect(char *sock_domain);
+
+int ptyb_message_server(char *sock_domain, char *message);
+int ptyb_get_cid(char *sock_domain);
 
 #endif
